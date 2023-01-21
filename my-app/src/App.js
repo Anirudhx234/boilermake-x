48f03007e652da503ba37b74395dc4c3f80bca43
Arnav <arnav.naval@gmail.com>
2023-01-20 23:02:26 -0500

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import UploadButton from './button';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <UploadButton />
+      </div>
+
+      <header className="App-header">
+        <p>
+          
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
